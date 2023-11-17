2a3e3f8aaa0c1326607873745bb6d4617150db33
sickCoder <lunagio683@gmail.com>
2023-11-17 12:17:50 -0800

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
 
 import './App.css';
 import Card from './components/Card';
+import youtubelogo from './components/images/youtube-logo.png'
+import chat from './components/images/chat.png'
+import githublogo from './components/images/githublogo.png'
+import schoolicon from './components/images/schoolicon.png'
 
 const websites = [
   {
     name:'Youtube',
     url: 'https://www.youtube.com',
-    imageSrc: 'youtube-logo.png',
+    imageSrc: youtubelogo,
     altText:'Youtube'
   },
   {
     name: 'GitHub',
     url: 'https://github.com',
-    imageSrc: 'github-logo.png',
+    imageSrc: githublogo,
     altText: 'GitHub',
   },
   {
     name: 'ChatGPT',
     url: 'https://www.chatgpt.com',
-    imageSrc: 'chatgpt-logo.png',
+    imageSrc: chat,
     altText: 'ChatGPT Open AI',
   },
   {
     name: 'LAVC Canvas',
     url: 'https://sso.laccd.edu/adfs/ls/idpinitiatedsignon.aspx?loginToRp=csprd.laccd.edu',
-    imageSrc: 'school-logo.png',
+    imageSrc: schoolicon,
     altText: 'LAVC Canvas',
   },
   // Add more websites as needed
@@ -33,7 +37,7 @@ const websites = [
 function App() {
   return (
     <div id="root">
-      <h4>Website cards for GitHub, ChatGPT, School, etc.</h4>
+      <h2>Welcome back </h2>
       <div className="card-grid">
         {websites.map((website, index) => (
           <Card key={index} {...website} />
