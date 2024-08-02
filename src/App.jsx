f49c22d71f6b0c86b53772821e2ccdbf39684c7b
sickCoder <lunagio683@gmail.com>
2024-08-02 14:53:52 -0700

    styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import youtubelogo from './components/images/youtube-logo.png'
 import chat from './components/images/chat.png'
 import githublogo from './components/images/githublogo.png'
 import schoolicon from './components/images/schoolicon.png'
-
+import hackerrank from './components/images/hackerrank.jpg'
+import codewars from './components/images/codewars.png'
 const websites = [
   {
     name:'Youtube',
@@ -31,13 +32,25 @@ const websites = [
     imageSrc: schoolicon,
     altText: 'LAVC Canvas',
   },
+  {
+    name:'Hacker Rank',
+    url: 'https://www.hackerrank.com/',
+    imageSrc: hackerrank,
+    altText: 'Hacker Rank'
+  },
+  {
+    name: 'Code Wars',
+    url: 'https://www.codewars.com/dashboard',
+    imageSrc: codewars,
+    altText: 'Code Wars'
+  },
   // Add more websites as needed
 ];
 
 function App() {
   return (
     <div id="root">
-      <h2>Welcome back </h2>
+      <h2>Welcome back</h2>
       <div className="card-grid">
         {websites.map((website, index) => (
           <Card key={index} {...website} />
